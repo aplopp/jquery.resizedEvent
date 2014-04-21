de614869af00a94256904fac606d687fb6192232
Anthony Plopper <anthony@cloudwebteam.com>
2014-04-21 11:17:05 -0500

    Prevented child events from propogating.

--- jquery.resizedEvent.js
+++ jquery.resizedEvent.js
@@ -1,5 +1,6 @@
 ( function( $, window ){
 	var $window = $(window);
+	var id = 0;
 	$.fn.resizedEvent = function( options ){
 		var that = this;
 		var dataKey = 'resizedEvent';
@@ -14,77 +15,85 @@
 			},
 			interval: 100 // throttle speed of resized event
 		};
-		// if its already been initialized, then merge in any new options and halt
-		if( this.data( dataKey ) ){
-			var data = this.data( dataKey );
-			data.settings = $.extend( data.settings, options );
-			this.data( dataKey, data );
-			return this;
-		// otherwise, merge options and do all initial setup.
-		} else {
-			var data = {};
-			var settings = $.extend( defaults, options );
-			data.settings = settings;
-			this.data( dataKey, {
-				settings: settings
-			});
-		}
-		var last_width = false;
-		var last_height = false;
-		var last_breakpoint = false;
-		var current_width = this.outerWidth();
-		var current_height = this.outerHeight();
-		var current_breakpoint = getBreakpoint();
+		this.each( function(){
+			id++;
+			var $el = $(this);
+			// if its already been initialized, then merge in any new options and halt
+			if( $el.data( dataKey ) ){
+				var data = $el.data( dataKey );
+				data.settings = $.extend( data.settings, options );
+				$el.data( dataKey, data );
+				return $el;
+			// otherwise, merge options and do all initial setup.
+			} else {
+				var data = {
+					id: id
+				};
+				var settings = $.extend( defaults, options );
+				data.settings = settings;
+				$el.data( dataKey, {
+					settings: settings
+				});
+			}
+			var last_width = false;
+			var last_height = false;
+			var last_breakpoint = false;
+			var current_width = $el.outerWidth();
+			var current_height = $el.outerHeight();
+			var current_breakpoint = getBreakpoint();
 
-		function getBreakpoint(){
-			for ( breakpoint in settings.breakpoints ){
-				if ( current_width > settings.breakpoints[ breakpoint ] ){
-					return breakpoint;
+			function getBreakpoint(){
+				for ( breakpoint in settings.breakpoints ){
+					if ( current_width > settings.breakpoints[ breakpoint ] ){
+						return breakpoint;
+					}
 				}
 			}
-		}
-		function getWidth(){
-			return current_width ;
-		}
-		function breakpointHasChanged(){
-			return last_breakpoint !== current_breakpoint ;
-		}
-		function horizontalHasChanged(){
-			return last_width !== current_width ;
-		}
-		function verticalHasChanged(){
-			return last_height !== current_height ;
-		}
-
-		var handlePossibleResize = function(){
-			last_width = current_width;
-			last_height = current_height;
-			last_breakpoint = current_breakpoint;
-			current_width = that.outerWidth();
-			current_height = that.outerHeight();
-			current_breakpoint = getBreakpoint();
-			var info = {
-				breakpoint: current_breakpoint,
-				h: current_height,
-				w: current_width,
-				from: {
-					breakpoint: last_breakpoint,
-					h: last_height,
-					w: last_width,
-				},
-				changed: {
-					w: horizontalHasChanged(),
-					h: verticalHasChanged(),
-					breakpoint: breakpointHasChanged()
-				}
-			};
-			if ( info.changed.h || info.changed.w ){
-				that.trigger( 'resized', info );
-				if ( info.changed.w ){ that.trigger( 'resized-w', info ); }
-				if ( info.changed.h ){ that.trigger( 'resized-h', info ); }
+			function getWidth(){
+				return current_width ;
 			}
-		}
-		$window.on( 'resize load', $.throttle( settings.interval, handlePossibleResize ) );
+			function breakpointHasChanged(){
+				return last_breakpoint !== current_breakpoint ;
+			}
+			function horizontalHasChanged(){
+				return last_width !== current_width ;
+			}
+			function verticalHasChanged(){
+				return last_height !== current_height ;
+			}
+
+			var handlePossibleResize = function(){
+				last_width = current_width;
+				last_height = current_height;
+				last_breakpoint = current_breakpoint;
+				current_width = that.outerWidth();
+				current_height = that.outerHeight();
+				current_breakpoint = getBreakpoint();
+				var info = {
+					breakpoint: current_breakpoint,
+					h: current_height,
+					w: current_width,
+					from: {
+						breakpoint: last_breakpoint,
+						h: last_height,
+						w: last_width,
+					},
+					changed: {
+						w: horizontalHasChanged(),
+						h: verticalHasChanged(),
+						breakpoint: breakpointHasChanged()
+					}
+				};
+				if ( info.changed.h || info.changed.w ){
+					$el.triggerHandler( 'resized', info );
+					if ( info.changed.w ){ $el.triggerHandler( 'resized-w', info ); }
+					if ( info.changed.h ){ $el.triggerHandler( 'resized-h', info ); }
+				}
+			}
+			$window.unbind( 'resize.el-'+data.id );
+			$window.on( 'resize.el-'+data.id, $.throttle( settings.interval, handlePossibleResize ));
+		});
+		// $window.on( 'load', handlePossibleResize );
 		return this;
 	}
 }( jQuery, window ) );
